21feec962098cce23a5262ef204be68e3fcf9345
shanoaice <25174488+shanoaice@users.noreply.github.com>
2023-03-13 19:56:30 +0800

    feature(Vector): FromRustString Method

--- src/Vector.fs
+++ src/Vector.fs
@@ -4,13 +4,11 @@
 
 open System
 open System.Runtime.InteropServices
+open Rhodopsin.NativeInterop.String
 
 /// <summary>
 /// Represents raw parts of a Rust <c>Vec&lt;T&gt;</c>. Viable for other FFI too as long as the format is obeyed.
 /// </summary>
-/// <param name="vecBuffer">The pointer to the Vector buffer.</param>
-/// <param name="vecLength">The length of the Vector.</param>
-/// <param name="vecCapacity">The capacity of the Vector (can contain <c>len &lt;= capacity</c> without realloc).</param>
 [<Struct; StructLayout(LayoutKind.Sequential)>]
 type Vector(vecBuffer: nativeint, vecLength: unativeint, vecCapacity: unativeint) =
     /// The pointer that points to the content of the Vector.
@@ -33,7 +31,7 @@ type Vector(vecBuffer: nativeint, vecLength: unativeint, vecCapacity: unativeint
         new Span<'T>(NativeInterop.NativePtr.toVoidPtr rawPtr, int this.Length)
 
     /// <summary>
-    /// Represents the underlying <c>Vec&lt;T&gt;</c> data as a <c>System.Span&lt;T&gt;</c>, including the empty space at the end.<br/>
+    /// Represents the underlying <c>Vec&lt;T&gt;</c> data as a <c>System.Span&lt;T&gt;</c>, including empty spaces at the end.<br/>
     /// Due to CLR limit, you will not be able to access any element beyond <c>Int32.MaxValue</c>.
     /// </summary>
     member this.AsSpanAll<'T when 'T: unmanaged>() =
@@ -53,10 +51,10 @@ type Vector(vecBuffer: nativeint, vecLength: unativeint, vecCapacity: unativeint
         new ReadOnlySpan<'T>(NativeInterop.NativePtr.toVoidPtr rawPtr, int this.Length)
 
     /// <summary>
-    /// Marshals strings inside the vector as C-style NUL terminated strings. When operating on a large amount of data, this might become a performance bottleneck. Consider converting the string into a vector of UTF-16 beforehand and use <see cref="M:Rhodopsin.NativeInterop.Vector.FromVecVecUTF16Char"/> instead.
+    /// Marshals strings inside the vector as C-style NUL terminated strings. When operating on a large amount of data, this might become a performance bottleneck. Consider converting the string into a vector of UTF-16 beforehand and use <see cref="M:Rhodopsin.NativeInterop.Vector.FromVecUTF16Char"/> instead.
     /// </summary>
     /// <returns>An <c>IEnumerable&lt;String&gt;</c> representing all the strings in the vector in original order.</returns>
-    member this.FromVecCStrAsPtr() =
+    member this.FromCStrAsPtr() =
         let rawPtr = NativeInterop.NativePtr.ofNativeInt<nativeint> this.Buffer
         let length = this.Length
 
@@ -66,10 +64,27 @@ type Vector(vecBuffer: nativeint, vecLength: unativeint, vecCapacity: unativeint
         }
 
     /// <summary>
+    /// Marshals strings inside the vector as Rust-style UTF-8 encoded strings. <b>This method preserves embedded NUL character.</b> When operating on a large amount of data, this might become a performance bottleneck. Consider converting the string into a vector of UTF-16 beforehand and use <see cref="M:Rhodopsin.NativeInterop.Vector.FromVecUTF16Char"/> instead.
+    /// </summary>
+    /// <returns>An <c>IEnumerable&lt;String&gt;</c> representing all the strings in the vector in original order.</returns>
+    member this.FromRustString() =
+        let rawPtr = NativeInterop.NativePtr.ofNativeInt<RustString> this.Buffer
+        let length = this.Length
+
+        seq {
+            for i in 0 .. (int length - 1) do
+                Marshal
+                    .PtrToStructure<RustString>(
+                        NativeInterop.NativePtr.add rawPtr i |> NativeInterop.NativePtr.toNativeInt
+                    )
+                    .ToString()
+        }
+
+    /// <summary>
     /// Marshals strings inside the vector as a vector of UTF-16 chars. This exists because native languages such as Rust and C++ can potentially encode strings faster than .NET CLR.
     /// </summary>
     /// <returns>An <c>IEnumerable&lt;String&gt;</c> representing all the strings in the vector in original order.</returns>
-    member this.FromVecVecUTF16Char() =
+    member this.FromVecUTF16Char() =
         let rawPtr = NativeInterop.NativePtr.ofNativeInt<Vector> this.Buffer
         let length = this.Length
 
