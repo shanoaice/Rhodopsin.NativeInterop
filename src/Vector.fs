c39468563bafe6a48f58ea14a9ec179eb197dec9
shanoaice <25174488+shanoaice@users.noreply.github.com>
2023-03-13 21:46:45 +0800

    docs(format): move notices to remarks section

--- src/Vector.fs
+++ src/Vector.fs
@@ -4,7 +4,6 @@
 
 open System
 open System.Runtime.InteropServices
-open Rhodopsin.NativeInterop.String
 
 /// <summary>
 /// Represents raw parts of a Rust <c>Vec&lt;T&gt;</c>. Viable for other FFI too as long as the format is obeyed.
@@ -21,9 +20,11 @@ type Vector(vecBuffer: nativeint, vecLength: unativeint, vecCapacity: unativeint
     member this.Capacity = vecCapacity
 
     /// <summary>
-    /// Represents the underlying <c>Vec&lt;T&gt;</c> data as a <c>System.Span&lt;T&gt;</c>.<br/>
-    /// Due to CLR limit, you will not be able to access any element beyond <c>Int32.MaxValue</c>.
+    /// Represents the underlying <c>Vec&lt;T&gt;</c> data as a <c>System.Span&lt;T&gt;</c>.
     /// </summary>
+    /// <remarks>
+    /// Due to CLR limit, you will not be able to access any element beyond <c>Int32.MaxValue</c>.
+    /// </remarks>
     member this.AsSpan<'T when 'T: unmanaged>() =
         let rawPtr = NativeInterop.NativePtr.ofNativeInt<'T> this.Buffer
         // int (this.len) might possibly truncate length
@@ -31,9 +32,11 @@ type Vector(vecBuffer: nativeint, vecLength: unativeint, vecCapacity: unativeint
         new Span<'T>(NativeInterop.NativePtr.toVoidPtr rawPtr, int this.Length)
 
     /// <summary>
-    /// Represents the underlying <c>Vec&lt;T&gt;</c> data as a <c>System.Span&lt;T&gt;</c>, including empty spaces at the end.<br/>
-    /// Due to CLR limit, you will not be able to access any element beyond <c>Int32.MaxValue</c>.
+    /// Represents the underlying <c>Vec&lt;T&gt;</c> data as a <c>System.Span&lt;T&gt;</c>, including empty spaces at the end.
     /// </summary>
+    /// <remarks>
+    /// Due to CLR limit, you will not be able to access any element beyond <c>Int32.MaxValue</c>.
+    /// </remarks>
     member this.AsSpanAll<'T when 'T: unmanaged>() =
         let rawPtr = NativeInterop.NativePtr.ofNativeInt<'T> this.Buffer
         // int (this.len) might possibly truncate length
@@ -41,9 +44,11 @@ type Vector(vecBuffer: nativeint, vecLength: unativeint, vecCapacity: unativeint
         new Span<'T>(NativeInterop.NativePtr.toVoidPtr rawPtr, int this.Capacity)
 
     /// <summary>
-    /// Represents the underlying <c>Vec&lt;T&gt;</c> data as a <c>System.ReadOnlySpan&lt;T&gt;</c>.<br/>
-    /// Due to CLR limit, you will not be able to access any element beyond <c>Int32.MaxValue</c>.
+    /// Represents the underlying <c>Vec&lt;T&gt;</c> data as a <c>System.ReadOnlySpan&lt;T&gt;</c>.
     /// </summary>
+    /// <remarks>
+    /// Due to CLR limit, you will not be able to access any element beyond <c>Int32.MaxValue</c>.
+    /// </remarks>
     member this.AsReadOnlySpan<'T when 'T: unmanaged>() =
         let rawPtr = NativeInterop.NativePtr.ofNativeInt<'T> this.Buffer
         // int (this.len) might possibly truncate length
@@ -51,8 +56,11 @@ type Vector(vecBuffer: nativeint, vecLength: unativeint, vecCapacity: unativeint
         new ReadOnlySpan<'T>(NativeInterop.NativePtr.toVoidPtr rawPtr, int this.Length)
 
     /// <summary>
-    /// Marshals strings inside the vector as C-style NUL terminated strings. When operating on a large amount of data, this might become a performance bottleneck. Consider converting the string into a vector of UTF-16 beforehand and use <see cref="M:Rhodopsin.NativeInterop.Vector.FromVecUTF16Char"/> instead.
+    /// Marshals strings inside the vector as C-style NUL terminated strings.
     /// </summary>
+    /// <remarks>
+    /// When operating on a large amount of data, this might become a performance bottleneck. Consider converting the string into a vector of UTF-16 beforehand and use <see cref="M:Rhodopsin.NativeInterop.Vector.FromVecUTF16Char"/> instead.
+    /// </remarks>
     /// <returns>An <c>IEnumerable&lt;String&gt;</c> representing all the strings in the vector in original order.</returns>
     member this.FromCStrAsPtr() =
         let rawPtr = NativeInterop.NativePtr.ofNativeInt<nativeint> this.Buffer
@@ -64,8 +72,11 @@ type Vector(vecBuffer: nativeint, vecLength: unativeint, vecCapacity: unativeint
         }
 
     /// <summary>
-    /// Marshals strings inside the vector as Rust-style UTF-8 encoded strings. <b>This method preserves embedded NUL character.</b> When operating on a large amount of data, this might become a performance bottleneck. Consider converting the string into a vector of UTF-16 beforehand and use <see cref="M:Rhodopsin.NativeInterop.Vector.FromVecUTF16Char"/> instead.
+    /// Marshals strings inside the vector as Rust-style UTF-8 encoded strings.
     /// </summary>
+    /// <remarks>
+    /// <b>This method preserves embedded NUL character.</b> When operating on a large amount of data, this might become a performance bottleneck. Consider converting the string into a vector of UTF-16 beforehand and use <see cref="M:Rhodopsin.NativeInterop.Vector.FromVecUTF16Char"/> instead.
+    /// </remarks>
     /// <returns>An <c>IEnumerable&lt;String&gt;</c> representing all the strings in the vector in original order.</returns>
     member this.FromRustString() =
         let rawPtr = NativeInterop.NativePtr.ofNativeInt<RustString> this.Buffer
@@ -100,6 +111,5 @@ type Vector(vecBuffer: nativeint, vecLength: unativeint, vecCapacity: unativeint
 
                 String(charSpan)
         }
-        
-    static member FromEnumerable () =
-        ()
+
+    static member FromEnumerable() = ()
