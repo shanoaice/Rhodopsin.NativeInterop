06f04345a0fb9237550ad51bdbf8d3edce68602a
shanoaice <25174488+shanoaice@users.noreply.github.com>
2023-03-13 21:43:40 +0800

    feature(stub): FreeHGlobal auto-export

---
+++ src/Free.fs
@@ -0,0 +1,8 @@
+﻿module Rhodopsin.NativeInterop.Free
+
+open System.Runtime.InteropServices
+
+// This currently does nothing due to lack of support from the F# compiler.
+[<UnmanagedCallersOnly(EntryPoint = "ffi_free")>]
+let freeHGlobal = Marshal.FreeHGlobal
+
