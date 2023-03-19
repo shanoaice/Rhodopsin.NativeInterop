a307c8f1f5f4a1a524efd4038a0408ea8c13a68a
shanoaice <25174488+shanoaice@users.noreply.github.com>
2023-03-19 23:21:12 +0800

    refactor(Destroy): consistent naming

---
+++ src/Destroy.fs
@@ -0,0 +1,8 @@
+﻿module Rhodopsin.NativeInterop.Destroy
+
+open System.Runtime.InteropServices
+
+// This currently does nothing due to lack of support from the F# compiler.
+[<UnmanagedCallersOnly(EntryPoint = "ffi_destroy")>]
+let freeHGlobal = Marshal.DestroyStructure
+
--- src/Free.fs
+++
@@ -1,8 +0,0 @@
-﻿module Rhodopsin.NativeInterop.Free
-
-open System.Runtime.InteropServices
-
-// This currently does nothing due to lack of support from the F# compiler.
-[<UnmanagedCallersOnly(EntryPoint = "ffi_free")>]
-let freeHGlobal = Marshal.FreeHGlobal
-
